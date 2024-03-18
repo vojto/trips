056602ecfcfeb35d5e47ef442ddf38dc8c1cedf1
Vojtech Rinik <vojto@rinik.net>
2024-03-18 10:40:00 +0100

    making firebase work

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "./db";
+
 export function App() {
-  const addTrip = () => {
-    const name = prompt("Name of the trip:");
+  const [trips, setTrips] = useState<{ destination: string }[]>([]);
+
+  useEffect(() => {
+    const dispose = onSnapshot(collection(db, "trips"), (doc) => {
+      const trips = doc.docs.map(
+        (doc) => doc.data() as { destination: string }
+      );
+
+      setTrips(trips);
+    });
+
+    return () => {
+      dispose();
+    };
+  }, []);
+
+  const addTrip = async () => {
+    const destination = prompt("Destination of the trip:");
+
+    await addDoc(collection(db, "trips"), {
+      destination,
+    });
   };
 
   return (
     <div className="max-w-[800px] m-auto bg-white px-10 py-10 min-h-screen space-y-2">
-      <h1 className="text-2xl font-bold">My trips</h1>
+      <h1 className="text-2xl font-bold">Recent trips</h1>
+
       <div>
         <a onClick={addTrip}>Add a trip</a>
       </div>
+
+      <ul>
+        {trips.map((trip, index) => (
+          <li key={index}>{trip.destination}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDBv8s_D1dtwZXzt4UBBe9fpvrTQo2dq58",
   authDomain: "trips-a8392.firebaseapp.com",
@@ -13,5 +10,5 @@ const firebaseConfig = {
   appId: "1:443503021041:web:9c99b1f647a3f80c37d250",
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
